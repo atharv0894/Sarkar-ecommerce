fea3d844a7d44429d493009becd767e5e074f85f
atharv0894 <shindeatharv36@gmail.com>
2026-06-21 22:54:12 +0530

    Fix Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { Server } from 'lucide-react'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    Server:{port:5173}
     },
+  },
+  server: {
+    port: 5173,
   },
 })
